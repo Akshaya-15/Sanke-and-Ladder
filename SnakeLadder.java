0dcee4d3ede2247afa11760e2240879b386b3ab2
“Akshaya-15” <akshaya.adisht90@gmail.com>
2021-01-02 14:35:30 +0530

    for 0 position

---
+++ SnakeLadder.java
@@ -0,0 +1,18 @@
+import java.util.Queue;
+import java.lang.Math;
+//Uc1 for position 0
+public class SnakeLadder{
+private int start_position=0;
+private int end_position=100;
+
+	public SnakeLadder(int start_poistion,int end_position){
+		this.start_position=0;
+		this.end_position=100;
+}
+		public int getStart_position(){
+			return 0;
+}
+		public int getEnd_position(){
+		return 100;
+}
+}
